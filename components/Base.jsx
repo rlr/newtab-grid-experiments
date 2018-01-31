74d0f1722427a50928aa5d40b21f8443c8805190
Ricky Rosario <rickyrosario@gmail.com>
2018-01-31 12:42:26 -0500

    one vs two rows of top sites

--- components/Base.jsx
+++ components/Base.jsx
@@ -8,12 +8,17 @@ import {Highlights} from "./Highlights";
 export class Base extends React.PureComponent {
   constructor(props) {
     super(props);
+    this.onTopSitesRowsChange = this.onTopSitesRowsChange.bind(this);
     this.onTopSitesChange = this.onTopSitesChange.bind(this);
     this.onPocketChange = this.onPocketChange.bind(this);
     this.state = {
+      topsitesRows: 2,
       topsites: "smaller-topsites",
       pocket: "wider-columns"
     };
+  }
+  onTopSitesRowsChange(ev) {
+    this.setState({topsitesRows: parseInt(ev.target.value)});
   }
   onTopSitesChange(ev) {
     this.setState({topsites: ev.target.value});
@@ -24,6 +29,10 @@ export class Base extends React.PureComponent {
   render() {
     return (<div className="outer-wrapper">
       <div className="top-bar">
+        <select onChange={this.onTopSitesRowsChange}>
+          <option value="1">1 row of Top Sites</option>
+          <option value="2" selected="selected">2 rows of Top Sites</option>
+        </select>
         <select onChange={this.onTopSitesChange}>
           <option value="smaller-topsites">Smaller Top Sites</option>
           <option value="normal-topsites">Normal Top Sites</option>
@@ -34,9 +43,9 @@ export class Base extends React.PureComponent {
           <option value="more-columns">More pocket columns</option>
         </select>
       </div>
-      <main className={`${this.state.topsites} ${this.state.pocket}`}>
+      <main className={`${this.state.topsites} ${this.state.pocket} ${this.state.topsitesRows === 1 ? "topsites-one-row" : ""}`}>
         <Search />
-        <TopSites rows={2} />
+        <TopSites rows={this.state.topsitesRows} />
         <div className="sections-list">
           <Pocket rows={2} />
           <Highlights />
